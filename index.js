d5dac921c9c5dcc819265e2c9b40898557007048
Vithushan <vithushan@Vithushans-MacBook-Pro.local>
2021-03-12 21:12:15 -0500

    finalized version

--- index.js
+++ index.js
@@ -6,119 +6,228 @@ const db = require('./app/connection')('company_db', 'password')
 
 // The first step is to ask the user questions 
 
-function firstQuestion(){
-   return inquirer.prompt([
-        {name:"initalQuestion", message: "What would you like to do today?", type: "list", choices: ["View", "Add", "Update employee roles"] }
-   ])
+function firstQuestion() {
+    return inquirer.prompt([
+        { name: "initalQuestion", message: "What would you like to do today?", type: "list", choices: ["View", "Add", "Update employee roles"] }
+    ])
 
 }
 
-// If the user chooses to view 
+// USER CHOOSES TO VIEW OPTIONS 
 
-async function viewOptions(){
+async function viewOptions() {
     return inquirer.prompt([
-        {name:"viewQuestion", mesaage: "What would you like to view?", type: "list", choices: ["departments", "roles", "employees"]}
-               
+        { name: "viewQuestion", mesaage: "What would you like to view?", type: "list", choices: ["departments", "roles", "employees"] }
+
     ])
 }
 
 //1.) if they chooose to view from departments 
-async function viewDepartment(){
+async function viewDepartment() {
     let department = await db.query("SELECT * FROM department")
-    console.log( department);
+    console.log(department);
     return department;
 }
 
 
 //2.) if they choose to view  role 
 
-async function viewRole(){
+async function viewRole() {
     let role = await db.query("SELECT * FROM role")
-    return role 
+    return role
 }
 
 // 3.) if they want to view employees
 
-async function viewEmployee(){
-    let employee= await db.query("SELECT * FROM employee ")
-    return employee 
+async function viewEmployee() {
+    let employee = await db.query("SELECT * FROM employee ")
+    return employee
 }
 // **********************************************************************************************************************************
-//if the user chooses to add  
+//USER CHOOSES TO ADD
 
-async function addOption(){
+async function addOption() {
     return inquirer.prompt([
-        {name:"addQuestion", message: "what would you like to add?", type: "list", choices: ["departments, roles, employees"]}
+        { name: "addQuestion", message: "what would you like to add?", type: "list", choices: ["departments", "roles", "employees"] }
     ])
 }
 
 //1.) add department 
-async function addDepartment(){
-    return inquirer.prompt([
-        {name:"addDepartment", message: "what department would you like to add?", type:"input"}
+async function addDepartment() {
+    var departmentInput = await inquirer.prompt([
+        { name: "addDepartment", message: "what department would you like to add?", type: "input" }
+
     ])
 
+    console.log(departmentInput)
+    let newValue = await db.query(`INSERT INTO department VALUES(0,"${departmentInput.addDepartment}")`) 
+    let result = await db.query(`SELECT * FROM department`)
+    return result
 }
 
 // 2.) add roles 
-async function addRoles(){
-    return inquirer.prompt([
-        {name:"addRoles", message: "What role would you like to add?", type: "input"}
+async function addRoles() {
+    var roleInput = await inquirer.prompt([
+        { name: "addRole", message: "What role would you like to add?", type: "input" }
     ])
+    var salaryInput = await inquirer.prompt([
+        {name:"addSalary", message:"What would the salary be?", type: "input" },
+    
+    ])
+    var departmentInput = await inquirer.prompt([
+        {name: "addDepartment", message:"What department are they part of?", type:"input"}
+    ])
+
+    console.log(roleInput)
+    console.log(salaryInput)
+    console.log(departmentInput)
+    let newValue1= await db.query(`INSERT INTO role VALUES (0,"${roleInput.addRole}", "${salaryInput.addSalary}", "${departmentInput.addDepartment}")`)
+    let result = await db.query(`SELECT * FROM role`)
+
+    return result
+
 }
 
 //3.) add employees 
-async function addEmployees(){
-    return inquirer.prompt([
-        {name: "addEmployee", message:"What employee would you like to add", type:"list", choices: ["accountant", "marketing", "human resource"]}
+async function addEmployees() {
+    var firstName= await inquirer.prompt([
+        { name: "addFirst", message: "What is the employee's first name?", type: "input" }
     ])
+    var lastName= await inquirer.prompt([
+        { name: "addLast", message: "What is the employee's last name?", type: "input" }
+    ])
+    var roleId= await inquirer.prompt([
+        { name: "addRoleId", message: "What is the employee's role id?", type: "input" }
+    ])
+    var managerID= await inquirer.prompt([
+        { name: "addManager", message: "Is this employee also manager? (1 for no, 2 for yes)", type: "input" }
+    ])
+
+    console.log(firstName)
+    console.log(lastName)
+    console.log(roleId)
+    console.log(managerID)
+    let value2= await db.query(`INSERT INTO employee VALUES (0,"${firstName.addFirst}", "${lastName.addLast}", "${roleId.addRoleId}", "${managerID.addManager}")`)
+    let result = await db.query(`SELECT * FROM employee`)
+    return result
 }
+
+//****************************************************************************************************************************
 
 // if the user chooses to update information 
 
-async function updateOption(){
-    return inquirer.prompt([
-        {name:"updateQuestion", message: "who would you like to update?", type: "list", choices: ["accountant", "marketing", "human resource"]}
+async function updateOption() {
+    var choice = inquirer.prompt([
+        { name: "updateQuestion", message: "Which employee would you like to update?", type: "list", choices: [db.query(`SELECT * FROM employee`)] }
     ])
+
+    console.log(choice)
 }
 
-function backContinue(){
+
+// to go back a step 
+function backContinue() {
     return inquirer.prompt([
-        {name:"action", message: "Would you like to continue?", type:"confirm" }
+        { name: "action", message: "Would you like to continue?", type: "confirm" }
     ])
 }
 // Logic to decide what the user wishes to view 
-async function main(){
+async function main() {
     const result = await firstQuestion()
     console.table(result)
-        if (result.initalQuestion === "View"){
-       let data= await viewOptions()
-       console.log('[data]',data)
+    if (result.initalQuestion === "View") {
+        let data = await viewOptions()
+        console.log('[data]', data)
 
-       if (data.viewQuestion === "departments"){
-           let data = await viewDepartment()
-           console.table(data)
-           let result = await backContinue()
-           console.log(result)
-        if (result.action=== true){
-            main() 
-        } 
+        if (data.viewQuestion === "departments") {
+            let data = await viewDepartment()
+            console.table(data)
+            let result = await backContinue()
+            console.log(result)
+
+            if (result.action === true) {
+                main()
+            }
             return;
-        
-       } else if (data.viewQuestion === "roles"){
-           console.log('im here')
-           let data = await viewRole() 
-           console.table(data)
-          
-       } else {
-       console.log('check')
-       let data3 = await viewEmployee()
-       console.table(data3)
-       return};
 
+        } else if (data.viewQuestion === "roles") {
+            console.log('Showing all company roles')
+            let data = await viewRole()
+            console.table(data)
+            let result = await backContinue()
+            console.log(result)
+
+            if (result.action === true) {
+                main()
+            } return;
+
+        } else {
+            console.log('Showing all company employees')
+            let data3 = await viewEmployee()
+            console.table(data3)
+            let result = await backContinue()
+            console.log(result)
+
+            if (result.action === true) {
+                main()
+                return
+            };
         } return;
+    }
+     if (result.initalQuestion === "Add") {
+        let data = await addOption()
+        console.log(data.addQuestion)
 
+        if (data.addQuestion === 'departments') { 
+            let result = await addDepartment()
+            console.table(result)
 
-} 
+            let move = await backContinue()
+            console.log(result)
+
+            if (move.action === true) {
+                main()
+            }
+            return;
+        }
+        else if (data.addQuestion ==='roles' ){
+        let result1 = await addRoles()
+        console.log(result1)
+
+        let move1 = await backContinue()
+            console.log(result)
+
+            if (move1.action === true) {
+                main()
+        } return; 
+    }
+    else {
+
+        let result2 = await addEmployees()
+        console.log(result2)
+
+        let move1 = await backContinue()
+            console.log(result)
+
+            if (move1.action === true) {
+                main()
+        } return; 
+    }
+}
+if (result.initalQuestion === "Update employee roles"){
+    let result3 = await updateOption()
+    console.log(result3)
+
+    let move2 = await backContinue()
+    console.log(result)
+
+    if (move2.action === true) {
+        main()
+} return; 
+
+}
+
+}
+
 
 main();
--- schema.sql
+++ schema.sql
@@ -4,11 +4,17 @@ CREATE DATABASE company_db;
 USE company_db; 
 
 
+
 CREATE TABLE department (
-id INT PRIMARY KEY, 
+id INT AUTO_INCREMENT PRIMARY KEY, 
 name VARCHAR(30) 
 ); 
 
+INSERT INTO department VALUES (1, "accounting"); 
+INSERT INTO department VALUES (2, "marketing"); 
+INSERT INTO department VALUES (3, "human resource"); 
+INSERT INTO department VALUES (4, "manager"); 
+INSERT INTO department VALUES(0,'test');
 CREATE TABLE role (
 id INT PRIMARY KEY, 
 title VARCHAR(30), 
@@ -24,7 +30,29 @@ role_id INT,
 manager_id INT 
 ); 
 
+INSERT INTO employee VALUES (1, "Adam", "silver", 1, 1); 
+INSERT INTO employee VALUES (2, "Brother", "andrews",1, 2); 
+INSERT INTO employee VALUES (3, "Cider", "Vinegar", 1, 1); 
 
+SELECT * FROM department;
 
- 
+-- 1 means employee 
+-- 2 means 
+
+-- accounting - 1 
+-- marketing - 2 
+-- hr - 3 
+INSERT INTO role VALUES (1, "accountant", 50000.00, 1); 
+INSERT INTO role VALUES (2, "accounting associate", 30000.00, 1); 
+INSERT INTO role VALUES (3, "Marketing specialist", 50000.00, 2); 
+INSERT INTO role VALUES (4, "Marketing Associate", 40000.00, 2); 
+INSERT INTO role VALUES (5, "human resource specialist", 50000.00, 2); 
+INSERT INTO role VALUES (6, "human resource assoicate", 40000.00, 3);
+INSERT INTO role VALUES (7, "Manager", 50000.00, 4);
+INSERT INTO role VALUES (8, "enginner", 60000.00, 3);
+
+SELECT * FROM employee;
+SELECT * FROM role;
+SELECT * FROM department; 
+
 
